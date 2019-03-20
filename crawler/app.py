14078840a6d759e222fa81df3453d50143cc7439
alexNeto <alex.neto3@hotmail.com>
2019-03-19 21:59:11 -0300

    refat setup

--- crawler/app.py
+++
@@ -1,6 +0,0 @@
-def main_loop():
-    pass
-
-
-if __name__ == "__main__":
-    main_loop()
--- crawler/database_connection/__init__.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://127.0.0.1:27017/lada")
-db = client.admin
