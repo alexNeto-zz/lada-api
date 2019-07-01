3f8a81849d66dc87bcc666608480b31bb3cff004
alexNeto <alex.neto3@hotmail.com>
2019-07-01 20:48:58 -0300

    adicionado setup para testes unitarios

---
+++ test/mock_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TESTEteste(unittest.TestCase):
+    def setUp(self):
+        self.isAlwaysTrue = True
+
+    def testa_se_raspa_dados(self):
+        self.assertTrue(self.isAlwaysTrue)
+
+    def testa_se_raspa_data(self):
+        self.assertTrue(self.isAlwaysTrue)
