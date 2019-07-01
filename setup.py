dd2c75737a558a6f52a50843a733c1a0923d686f
alexNeto <alex.neto3@hotmail.com>
2019-07-01 20:39:06 -0300

    adicionado setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lada-api',
+    version='0.0.0',
+    url='https://github.com/alexNeto/lada-api',
+    license='MIT License',
+    author='Alexandre Neto',
+    author_email='alex.neto3@hotmail.com',
+    description='',
+    packages=find_packages(),
+    install_requires=[],
+    test_suite='test',
+)
