17d2a48ff0cd563ffe4625ff4e543a85e93da740
alexNeto <alex.neto3@hotmail.com>
2019-07-01 19:59:46 -0300

    adicionado ci

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_pymongo import PyMongo
 
 from api.today_resume.today_resume import today_resume_blueprint
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/ladaDatabase"
+mongo = PyMongo(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(today_resume_blueprint, url_prefix="/today_resume")
