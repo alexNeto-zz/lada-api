85520a0e24d33394c00b53f21ce215f8fa832272
alexNeto <alex.neto3@hotmail.com>
2019-07-01 21:46:54 -0300

    comentado conexao com banco

--- api/app.py
+++ api/app.py
@@ -1,13 +1,13 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
-from flask_pymongo import PyMongo
-import os
 
 from api.today_resume.today_resume import today_resume_blueprint
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/ladaDatabase"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/ladaDatabase"
+# mongo = PyMongo(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(today_resume_blueprint, url_prefix="/today_resume")
