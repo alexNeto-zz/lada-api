d28a7b43393021b7bd04c7dc7e8df021fc50c170
alexNeto <alex.neto3@hotmail.com>
2019-08-11 23:16:50 -0300

    #15 adiconado opção para ºC e ºF

--- api/services/weather_validator.py
+++ api/services/weather_validator.py
@@ -3,19 +3,19 @@ def temperature(value):
 
 
 def rain_probability(value):
-    return try_or_none(lambda: __validate_value(value))
+    return try_or_none(lambda: __validate_value(value) / 100)
 
 
 def __validate_value(value, minimum=0, maximum=100):
     to_validate = float(value)
     if minimum <= float(to_validate) <= maximum:
-        return value
+        return to_validate
     else:
-        return None
+        return 0
 
 
 def try_or_none(on_try):
     try:
         return on_try()
     except ValueError:
-        return None
+        return 0
