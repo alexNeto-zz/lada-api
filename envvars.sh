85759c1c98394d0d52399deca6a1ab03e1b02576
alexNeto <alex.neto3@hotmail.com>
2019-08-24 00:08:39 -0300

    test ci

--- envvars.sh
+++ envvars.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 echo "start"
-#{
-#  printf "env_variables:\n"
-#  printf "\tMONGODB_URI: %s\n" "$MONGODB_URI"
-#  printf "\tOPEN_WEATHER_TOKEN: %s\n" "$OPEN_WEATHER_TOKEN"
-#  printf "\tSENTRY_DSN: %s\n" "$SENTRY_DSN"
-#} >>app.yaml
+
+echo "injecting variables"
+{
+  printf "\nenv_variables:\n"
+  printf "\tMONGODB_URI: '%s'\n" "$MONGODB_URI"
+  printf "\tOPEN_WEATHER_TOKEN: '%s'\n" "$OPEN_WEATHER_TOKEN"
+  printf "\tSENTRY_DSN: '%s'\n" "$SENTRY_DSN"
+} >>app.yaml
 echo "end"
