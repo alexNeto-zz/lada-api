064a777248af590a47470d5b721939f63ad2af2d
alexNeto <alex.neto3@hotmail.com>
2019-08-23 23:53:22 -0300

    test ci

--- envvars.sh
+++ envvars.sh
@@ -2,8 +2,8 @@
 echo "start"
 {
   printf "env_variables:\n"
-  printf "  MONGODB_URI: %s\n" "$MONGODB_URI"
-  printf "  OPEN_WEATHER_TOKEN: %s\n" "$OPEN_WEATHER_TOKEN"
-  printf "  SENTRY_DSN: %s\n" "$SENTRY_DSN"
+  printf "\tMONGODB_URI: %s\n" "$MONGODB_URI"
+  printf "\tOPEN_WEATHER_TOKEN: %s\n" "$OPEN_WEATHER_TOKEN"
+  printf "\tSENTRY_DSN: %s\n" "$SENTRY_DSN"
 } >>app.yaml
 echo "end"
