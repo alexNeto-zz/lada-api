8e5da25a79993c010e3147a791a7165be5fb0536
alexNeto <alex.neto3@hotmail.com>
2019-08-24 17:02:47 -0300

    test ci

--- varenv.sh
+++ varenv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo -e "injecting vars"
-
+echo -e "runtime: python37" >>app.yaml
 echo -e "env_variables:" >>app.yaml
 echo -e "  MONGODB_URI: $MONGODB_URI" >>app.yaml
 echo -e "  OPEN_WEATHER_TOKEN: $OPEN_WEATHER_TOKEN" >>app.yaml
