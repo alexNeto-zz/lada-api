78cc653df4b20aa7eaaab245447b6cf1ead1736e
alexNeto <alex.neto3@hotmail.com>
2019-08-24 16:47:30 -0300

    test ci

--- varenv.sh
+++ varenv.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-ls
+echo -e "injecting vars"
+
+echo -e "env_variables:" >>app.yaml
+echo -e "\n" >>app.yaml
+echo -e "\tMONGODB_URI: $MONGODB_URI\n" >>app.yaml
+echo -e "\tOPEN_WEATHER_TOKEN: $OPEN_WEATHER_TOKEN\n" >>app.yaml
+echo -e "\tSENTRY_DSN: $SENTRY_DSN" >>app.yaml
+
+cat app.yaml
+echo -e "done injecting"
