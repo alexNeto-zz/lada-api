9e551c78effa13de5f6f1ae87dcdaff41b10ec5a
alexNeto <alex.neto3@hotmail.com>
2019-08-23 22:15:27 -0300

    test migration heroku to appengine

---
+++ envvars.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+{
+  printf "env_variables:\n"
+  printf "\tMONGODB_URI=%s\n" "$MONGODB_URI"
+  printf "\tOPEN_WEATHER_TOKEN=%s\n" "$OPEN_WEATHER_TOKEN"
+  printf "\tSENTRY_DSN=%s\n" "$SENTRY_DSN"
+} >>app.yaml
