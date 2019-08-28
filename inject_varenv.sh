685040fa635380f5243a9e2adfc1e0b1eb6693d9
alexNeto <alex.neto3@hotmail.com>
2019-08-28 02:31:05 -0300

    test ci

--- inject_varenv.sh
+++ inject_varenv.sh
@@ -4,10 +4,10 @@ echo "injecting varenv"
 
 {
   printf "\nenv_variables:\n"
-  printf "\tMONGODB_URI: '%s'\n" "$MONGODB_URI"
-  printf "\tOPEN_WEATHER_TOKEN: '%s'\n" "$OPEN_WEATHER_TOKEN"
-  printf "\tSENTRY_DSN: '%s'\n" "$SENTRY_DSN"
-  printf "\tTESTE: '%s'\n" "AAAAAAAAAAA"
+  printf "  MONGODB_URI: '%s'\n" "$MONGODB_URI"
+  printf "  OPEN_WEATHER_TOKEN: '%s'\n" "$OPEN_WEATHER_TOKEN"
+  printf "  SENTRY_DSN: '%s'\n" "$SENTRY_DSN"
+  printf "  TESTE: '%s'\n" "AAAAAAAAAAA"
 } >>app.yaml
 
 echo "done"
