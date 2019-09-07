40f021559433813bf09a2c7cf73950376758471a
alexNeto <alex.neto3@hotmail.com>
2019-09-06 22:13:03 -0300

    remove PORT envvar

--- main.py
+++ main.py
@@ -14,5 +14,4 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 EndPoints(app).add_resources()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
