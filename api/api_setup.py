7806e67449860a622d05fb14d213b4975e3d9716
alexNeto <alex.neto3@hotmail.com>
2019-10-27 14:27:16 -0300

    update dependencies

--- api/api_setup.py
+++ api/api_setup.py
@@ -2,6 +2,7 @@ import os
 
 import sentry_sdk
 from mongoengine import connect
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from api.about.about_service import AboutService
 
@@ -13,4 +14,8 @@ def global_init():
         host=os.environ.get('MONGODB_URI', 'mongodb://localhost:27017')
     )
 
-    sentry_sdk.init(dsn=os.environ.get('SENTRY_DSN', ''), release="lada-api@" + AboutService().api_version())
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_DSN', ''),
+        release="lada-api@" + AboutService().api_version(),
+        integrations=[FlaskIntegration()]
+    )
